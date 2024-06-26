d49b717ffb228d1b50afa0a3301020c63786a4cb
Abiud-lang <abiud.kiprotich@student.moringaschool.com>
2024-06-26 16:03:07 +0300

    functions

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function saturdayFun(event = 'roller-skate') {
+    return `This Saturday, I want to ${event}!`
+
+  }
+  const mondayWork = function(event = 'go to the office') {
+    return `This Monday, I will ${event}.`
+    
+  }
+  function wrapAdjective(wrapper) {
+    return function(adjective) {
+      return `You are ${wrapper}${adjective}${wrapper}!`
+    }
+  }
+  
